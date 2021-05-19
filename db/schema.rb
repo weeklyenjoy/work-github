688ba315588533966fc6903b9c6999d02abf74a3
EC2 Default User <ec2-user@ip-172-31-22-88.ec2.internal>
2021-05-19 14:03:36 +0000

    修正箇所統合

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20210519015522) do
-=======
-ActiveRecord::Schema.define(version: 20210518132340) do
->>>>>>> 622e1b4caada880051b0099149eab0c330a0e59f
+ActiveRecord::Schema.define(version: 2021_05_19_015522) do
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   create_table "items", force: :cascade do |t|
-    t.string   "name"
-    t.text     "caption"
-    t.integer  "tax_out_price"
-    t.string   "image_id"
-    t.boolean  "is_active"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string "name"
+    t.text "caption"
+    t.integer "tax_out_price"
+    t.string "image_id"
+    t.boolean "is_active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-=======
->>>>>>> 622e1b4caada880051b0099149eab0c330a0e59f
 end
