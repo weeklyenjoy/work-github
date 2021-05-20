330359c82a0352902fcc0341bbd6dbd5c826c4ef
EC2 Default User <ec2-user@ip-172-31-22-88.ec2.internal>
2021-05-20 12:25:22 +0000

    ジャンル追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "homes#top"
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
 end
