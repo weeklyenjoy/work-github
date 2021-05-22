b4111d6c6241610c7cc1df32d89236a60064be7d
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-05-22 09:39:06 +0000

    注文履歴一覧追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,7 @@
 class Admin::OrdersController < ApplicationController
+    
+  def index
+    @orders = Order.page(params[:page]).per(10)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
   end
 
 end
