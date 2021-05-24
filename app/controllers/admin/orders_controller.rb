d8ebf76870ec54549960f08228a2fa38f9615f18
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-05-24 02:11:14 +0000

    カート直し

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,16 @@
 class Admin::OrdersController < ApplicationController
-    
+  
   def index
-    @orders = Order.page(params[:page]).per(10)
+  	@orders = Order.all
+  end
+
+  def show
+  	@order = Order.find(params[:id])
   end
   
+  private
+
+  def orders_params
+    params.require(:order).permit(:orders_status)
+  end
 end
