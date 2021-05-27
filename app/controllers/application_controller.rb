b523f41645028d04678292f64474845380770d04
EC2 Default User <ec2-user@ip-172-31-87-130.ec2.internal>
2021-05-27 02:52:39 +0000

    [finish]adminitem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number])
   end
